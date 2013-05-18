c1d8fd8a0fe54c9a3f438a3c08661c465be07a2d
Liuqingzhi <liuqingzhi@liuqingzhi.com>
2013-05-19 00:10:00 +0800

    使用derby数据库可以运行起来了

---
+++ src/main/java/com/yesmynet/database/query/utils/DatabaseIniter.java
@@ -0,0 +1,60 @@
+package com.yesmynet.database.query.utils;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.ibatis.SqlMapClientCallback;
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+import com.ibatis.sqlmap.client.SqlMapExecutor;
+/**
+ * 初始始化数据库
+ * @author 刘庆志
+ *
+ */
+public class DatabaseIniter extends SqlMapClientDaoSupport 
+{
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	/**
+	 * 初始化数据库的方法
+	 */
+	public void init()
+	{
+		boolean inited=dbinited();
+		if(!inited)
+		{
+			initDB();
+		}
+	}
+	private boolean dbinited()
+	{
+		boolean re=true;
+		
+		Map queryForObject=null;
+		try
+		{
+			queryForObject = (Map)this.getSqlMapClientTemplate().queryForObject("getDBInitedData");
+		}
+		catch (Exception e)
+		{
+			logger.debug("查询出的数据=",e);//JSONObject.fromObject(queryForObject));
+			re=false;
+		}
+		logger.debug("查询出的数据=",JSONObject.fromObject(queryForObject));
+		
+		return re;
+		
+	}
+	private void initDB()
+	{
+		this.getSqlMapClientTemplate().update("createTableSysInit");
+		
+	}
+	
+}

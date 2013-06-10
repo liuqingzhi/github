1b0126ac17f0375a486603294b8ef667d063c9f0
Liuqingzhi <liuqingzhi@liuqingzhi.com>
2013-06-10 21:40:03 +0800

    可以正确的用;分隔SQL了

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,27 +9,109 @@ import java.util.regex.Pattern;
 import junit.framework.TestCase;
 
 public class SimpleTest extends TestCase {
-	public void testDoInQuery() {
-		String sql = "abc/*'aaa*/'bc';;'/*'*/'\n--comment'\n\n'/**'comment'";
-		splitSQL(sql);
-	}
-
-	private List<String> splitSQL(String sql) {
+	public void testSplitSQL() {
 		
-
-		System.out.println("src=" + sql);
-		List<String> re = new ArrayList<String>();
 		Set<SignPair> pairs = new HashSet<SignPair>();
 
 		pairs.add(new SignPair("'", "'"));
 		pairs.add(new SignPair("/\\*", "\\*/"));
-		pairs.add(new SignPair("--", "\\n"));
-
+		pairs.add(new SignPair("--", "\n"));
+		final String splitter=";";
 		
-		String allSignPairStart = getALLSignPair(pairs,0);
-		getSignPairMatchList(pairs,sql,1);
+		List<String> splitSQLs=null;
+		String sql ="";
+		
 		
 
+		sql="select * From m_order_info";
+		splitSQLs = splitSQL(sql,pairs,splitter);
+		System.out.println(sql+"\n\n"+splitSQLs);
+		
+		sql="select * From m_member_info;;;";
+		splitSQLs = splitSQL(sql,pairs,splitter);
+		System.out.println(sql+"\n\n"+splitSQLs);
+		
+		sql="insert into m_order_info(a,b,c,d)values('a/*','*/b','c;','d;;');m_values;--这是注释;\nselect/*这也是注释*/a from m_order_info";
+		splitSQLs=splitSQL(sql,pairs,splitter);
+		System.out.println(sql+"\n\n"+splitSQLs);
+		
+		sql= "abc/*'aaa*/'bc';;'/*'*/'\n--comment'\n\n'/**'comment'";
+		splitSQLs= splitSQL(sql,pairs,splitter);
+		System.out.println(sql+"\n\n"+splitSQLs);
+	}
+	/**
+	 * 分隔SQL，使用指定的分隔符分开多条sql
+	 * @param sql 多条sql
+	 * @param pairs 在sql中有成对出现的标记符，如多行注释，单行注释，两个单引号等，在这些成对出现的标记中
+	 * 出现的分隔符不应该分隔SQL
+	 * @param splitter 分隔符，如，使用分号分隔sql，则本变量即为";"
+	 * @return 分隔后的多条sql
+	 */
+	private List<String> splitSQL(final String sql,final Set<SignPair> pairs,final String splitter) {
+		List<String> re=new ArrayList<String>();
+		String sqlSelf=sql;
+		String patternStr=getALLSignPair(pairs,0);
+		patternStr+="|"+splitter;//要在sql中查找的符号是所有的标记的开关和分隔符
+		Pattern pattern=Pattern.compile(patternStr);
+		Matcher matcher = pattern.matcher(sqlSelf);
+		boolean finished=false;
+		boolean findComma=false;
+		String currentSplittedSql="";
+		int i=0;
+		while(!finished)
+		{
+			if(findComma)
+			{
+				re.add(currentSplittedSql);
+				currentSplittedSql="";
+			}
+			matcher = pattern.matcher(sqlSelf);
+			if(matcher.find())
+			{
+				String group = matcher.group();
+				int end = matcher.end();
+				String tmp=sqlSelf.substring(0, end);
+				currentSplittedSql+=tmp;
+				sqlSelf=sqlSelf.substring(end);
+				System.out.println("\t\ti="+i+",group="+group+",end="+end+",currentSplittedSql="+currentSplittedSql+",sqlSefl="+sqlSelf);
+				
+				if(group.equals(splitter))
+				{
+					findComma=true;
+				}
+				else
+				{
+					findComma=false;
+					String endStr =this.getSignPairEnd(group, pairs);
+					
+					int indexOf =sqlSelf.length();
+					
+					Pattern endPattern = Pattern.compile(endStr);
+					Matcher matcher2 = endPattern.matcher(sqlSelf);
+					if(matcher2.find())
+					{
+						indexOf=matcher2.end();
+					}
+					
+					currentSplittedSql+=sqlSelf.substring(0,indexOf);
+					sqlSelf=sqlSelf.substring(indexOf);
+					
+					System.out.println("\t\ti="+i+",endStr="+ endStr +",indexOf="+indexOf+",currentSplittedSql="+currentSplittedSql+"sqlSefl="+sqlSelf);
+				}
+				
+			}
+			else
+			{
+				finished=true;
+			}
+			i++;
+		}
+		currentSplittedSql+=sqlSelf;
+		
+		if(currentSplittedSql.length()>0)
+			re.add(currentSplittedSql);
+		
+		
 		return re;
 	}
 	/**
@@ -41,7 +124,6 @@ public class SimpleTest extends TestCase {
 	private String getALLSignPair(Set<SignPair> pairs,int flag)
 	{
 		String allBeginSign = "";
-		List<SignPairRuntime> beginMatchList = new ArrayList<SignPairRuntime>();
 		
 		int i = 0;
 		for (SignPair p : pairs) {
@@ -61,29 +143,28 @@ public class SimpleTest extends TestCase {
 
 			i++;
 		}
-		System.out.println(allBeginSign);
 		return allBeginSign;
-/*
-		Pattern pattern = Pattern.compile(allBeginSign);
-		Matcher matcher = pattern.matcher(sql);
-		while (matcher.find()) {
-			String s =matcher.group(0);
-				
-			System.out.printf("group=%s,start=%s,end=%s\n", matcher.group(),matcher.start(), matcher.end());
-			
-			SignPairRuntime b=new SignPairRuntime();
-			SignPair spair=new SignPair();
-			spair.setStart(matcher.group());
-			
-			b.setSignPair(spair);
-			b.setStartIndex(matcher.start());
-			b.setEndIndex(matcher.end());
-			beginMatchList.add(b);
-		}
-		
-		return beginMatchList;*/
-	}
 
+	}
+	/**
+	 * 根据运行时得到的开始标记得到对应的结束标记
+	 * @param start 在sql中得到的开始标记
+	 * @param pairs 所有标记对
+	 * @return 该 start对应的结束标记
+	 */
+	private String getSignPairEnd(String start,Set<SignPair> pairs)
+	{
+		String re="";
+		for(SignPair p:pairs)
+		{
+			if(Pattern.matches(p.getStart(), start))
+			{
+				re=p.getEnd();
+				break;
+			}
+		}
+		return re;
+	}
 	/**
 	 * 表示成对出现的符号,如''，两个单引号就是成对出现的符号，其它 还有注释也是.
 	 * 
@@ -92,11 +173,11 @@ public class SimpleTest extends TestCase {
 	 */
 	private class SignPair {
 		/**
-		 * 开始标记
+		 * 开始标记，以正则表达式表示
 		 */
 		private String start;
 		/**
-		 * 结束标记
+		 * 结束标记，以正则表达式表示
 		 */
 		private String end;
 		/**
@@ -133,44 +214,5 @@ public class SimpleTest extends TestCase {
 			this.end = end;
 		}
 
-	}
-
-	/**
-	 * 成对出现的标记运行后的数据
-	 * 
-	 * @author 刘庆志
-	 * 
-	 */
-	private class SignPairRuntime {
-		/**
-		 * 成对出现的标记
-		 */
-		private SignPair signPair;
-		/**
-		 * 开始标记的位置
-		 */
-		private Integer startIndex;
-		/**
-		 * 结束标记的位置
-		 */
-		private Integer endIndex;
-		public SignPair getSignPair() {
-			return signPair;
-		}
-		public void setSignPair(SignPair signPair) {
-			this.signPair = signPair;
-		}
-		public Integer getStartIndex() {
-			return startIndex;
-		}
-		public void setStartIndex(Integer startIndex) {
-			this.startIndex = startIndex;
-		}
-		public Integer getEndIndex() {
-			return endIndex;
-		}
-		public void setEndIndex(Integer endIndex) {
-			this.endIndex = endIndex;
-		}
 	}
 }

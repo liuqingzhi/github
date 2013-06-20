06f89126181c1551916bf3949cdc70fbf6b515e0
Liuqingzhi <liuqingzhi@liuqingzhi.com>
2013-06-20 22:24:04 +0800

    默认查询的参数增加一些说明文字

--- src/main/java/com/yesmynet/database/query/service/QueryDefaultImpl.java
+++ src/main/java/com/yesmynet/database/query/service/QueryDefaultImpl.java
@@ -943,7 +943,7 @@ public class QueryDefaultImpl  implements Query
         
         Parameter p1=new Parameter();
         p1.setTitle("SQL脚本");
-        p1.setDescription("可以使用F8执行SQL");//参数的描述，可以显示在界面的
+        p1.setDescription("功能提示：1、可以使用F8执行SQL；2、可以选中一部分SQL执行，然后执行。");//参数的描述，可以显示在界面的
         p1.setHtmlType(ParameterHtmlType.textArea);
         p1.setCustomName(PARAM_SQL);
         p1.setStyle("width: 1000px; height: 200px;");

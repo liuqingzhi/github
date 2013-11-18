eb6ce099ac0c50a6913a7fba0901118184a86a06
liuqingzhi <liuqingzhi@haijie.com>
2013-11-18 21:24:44 +0800

    html输入框增加一个属性表示是否仅有一个值

--- src/main/java/com/yesmynet/database/query/core/dto/ParameterHtmlType.java
+++ src/main/java/com/yesmynet/database/query/core/dto/ParameterHtmlType.java
@@ -12,40 +12,47 @@ public  enum ParameterHtmlType
 	/**
 	 * 单行文本框
 	 */
-	InputText("单行文本","<input type=''text'' name=''{1}'' style=''{2}'' class=''{3}'' value=''{0}'' >"),
+	InputText("单行文本",true,"<input type=''text'' name=''{1}'' style=''{2}'' class=''{3}'' value=''{0}'' >"),
 	/**
 	 * 多行文本框
 	 */
-	TextArea("多行文本","<textarea name=''{1}'' style=''{2}'' class=''{3}''>{0}</textarea>"),
+	TextArea("多行文本",true,"<textarea name=''{1}'' style=''{2}'' class=''{3}''>{0}</textarea>"),
 	/**
      * html中的hidden输入控件 
      */
-    InputHidden("Hidden输入控件","<input type=''hidden'' name=''{1}''  style=''{2}'' class=''{3}'' value=''{0}'' >"),
+    InputHidden("Hidden输入控件",true,"<input type=''hidden'' name=''{1}''  style=''{2}'' class=''{3}'' value=''{0}'' >"),
     /**
      * html中的select输入控件 
      */
-    Select("下拉框","<select name=''{1}''  style=''{2}'' class=''{3}''>{0}</select>"),
+    Select("下拉框",false,"<select name=''{1}''  style=''{2}'' class=''{3}''>{0}</select>"),
     /**
 	 * 单选radio
 	 */
-	Radio("单选","<input type=''radio'' name=''{1}'' value=''{0}'' style=''{2}'' class=''{3}'' {5}>{4}</input>\n"),
+	Radio("单选",true,"<input type=''radio'' name=''{1}'' value=''{0}'' style=''{2}'' class=''{3}'' {5}>{4}</input>\n"),
 	/**
 	 * 多选checkbox
 	 */
-	Checkbox("多选","<input type=''checkbox'' name=''{1}'' value=''{0}'' style=''{2}'' class=''{3}'' {5}>{4}</input>\n")
+	Checkbox("多选",false,"<input type=''checkbox'' name=''{1}'' value=''{0}'' style=''{2}'' class=''{3}'' {5}>{4}</input>\n")
     ;
 	/**
 	 * 显示在界面上的类型名称
 	 */
     private String title;
     /**
+     * 是否只有一个值。
+     * 因为html中有些输入框有些只能输入一个值，有些可以输入多个值；前者如：单行文本框、多行文本框，后者如
+     * checkbox框，多选的下拉框。
+     */
+    private boolean onlyOneValue;
+    /**
      * 显示本类型的参数时的html模板
      */
     private String htmlTemplate;
-    private ParameterHtmlType(String title,String htmlTemplate)
+    private ParameterHtmlType(String title,boolean onlyOneValue,String htmlTemplate)
     {
         this.title=title;
         this.htmlTemplate=htmlTemplate;
+        this.onlyOneValue=onlyOneValue;
     }
     /**
      * 得到显示的名称
@@ -64,5 +71,13 @@ public  enum ParameterHtmlType
 	public String getHtmlTemplate() {
 		return htmlTemplate;
 	}
-    
+	/**
+	 * 是否只有一个值。
+	 * 因为html中有些输入框有些只能输入一个值，有些可以输入多个值；前者如：单行文本框、多行文本框，后者如
+     * checkbox框，多选的下拉框。
+	 * @return true表示只能有一个值，false表示可以有多个值
+	 */
+	public boolean isOnlyOneValue() {
+		return onlyOneValue;
+	}
 }
